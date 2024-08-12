7ed6848cf6be83697e2e289048fff791b8c08f5e
Levent Avgoeren <s90643@bht-berlin.de>
2024-08-12 13:50:46 +0200

    added fetch for random QUizes

--- src/BackendFetch/api.ts
+++ src/BackendFetch/api.ts
@@ -1,4 +1,9 @@
+import {fetchWithErrorHandling} from "./fetchWithErrorHandling";
 
-export function getRandomQuizCards(genre:String){
-
+export async function getRandomQuizCards(genre:String){
+const url= `${process.env.REACT_APP_API_SERVER_URL}/quizCard/randomQuestion/${genre}`;
+    const response = await fetchWithErrorHandling(url, {
+        credentials: "include" as RequestCredentials,
+    });
+    return response.json();
 }
